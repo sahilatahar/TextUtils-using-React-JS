ea9d3f590b551e39fb0bd6a58b4fdc838ad8b1f1
Sahil <sahilatahar2002@gmail.com>
2022-11-26 18:08:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Alert from './components/Alert';
 import About from './components/About';
-import { useHistory } from 'react-router-dom'
 
 import {
   BrowserRouter as Router,
@@ -12,12 +11,6 @@ import {
   Route,
 } from "react-router-dom";
 function App() {
-
-  const history = useHistory();
-
-  useEffect(() => {
-    history.push('Page2URL')
-  }, [history])
 
   const [mode, setmode] = useState("light");
   const [alert, setAlert] = useState(null);
