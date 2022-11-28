8e5f797ae3de4df4e7d88e0c10c0a4f85173194a
Sahil <sahilatahar2002@gmail.com>
2022-11-28 15:50:53 +0530

    changing

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
         <Navbar mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <Routes>
-          <Route exact path='/' element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert} />} />
+          <Route path='/' element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert} />} />
           <Route exact path='/about' element={<About mode={mode} />} />
         </Routes>
       </BrowserRouter>
